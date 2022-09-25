5f117b542ddcb12b55d981e49bf7e9b924b97f90
tsraut19 <113488154+tsraut19@users.noreply.github.com>
2022-09-25 10:34:44 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,15 @@
+class fact{
+public static void main(String args[]){
+
+int i,fact=1;
+int num=5;
+
+
+	for(i=1;i<=num;i++){
+
+	fact = fact*i;
+	}
+	 System.out.println("Factorial = "+fact);
+}
+
+}
